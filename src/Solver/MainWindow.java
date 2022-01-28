c9a0fd11f77433eded269d1ab8a1d2ad75da8159
Aman <Amandeep2ra@gmail.com>
2022-01-28 18:05:04 -0500

    GUI Files

---
+++ src/Solver/MainWindow.java
@@ -0,0 +1,20 @@
+package Solver;
+
+import javax.swing.*;
+
+public class MainWindow {
+    private JPanel MainWindow;
+    private JPanel CubeCanvas;
+    private JPanel PanelSettings;
+    private JButton ButtonSettings;
+    private JTextPane tpAnimationSpeed;
+    private JSlider sliderAnimationSpeed;
+    private JButton ButtonSet;
+    private JTextPane cubeColorsTextPane;
+    private JTextPane topTextPane;
+    private JTextPane bottomTextPane;
+    private JTextPane leftTextPane;
+    private JTextPane rightTextPane;
+    private JTextPane frontTextPane;
+    private JTextPane backTextPane;
+}
